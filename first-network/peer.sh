a968434e4ef03ba3a49a80d4908a6a14dd1d3411
Walker <srrain@outlook.com>
2019-06-10 13:03:06 +0800

    add peer.sh

---
+++ first-network/peer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CHANNEL_NAME="mychannel"
+CCNAME="mycc"
+CCVERSION="1.0"
+
+SCRIPT_PATH=${PWD}
+echo script in $SCRIPT_PATH
+
+export GODEBUG=netdns=go
+export FABRIC_CFG_PATH=$SCRIPT_PATH/sampleconfig
+
+setGlobals () {
+    ORDERADDRESS="orderer.example.com:7050"
+    ORDERER_CA="$SCRIPT_PATH/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    CORE_PEER_TLS_ROOTCERT_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt
+	CORE_PEER_TLS_KEY_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.key
+	CORE_PEER_LOCALMSPID=Org2MSP
+	CORE_PEER_TLS_CERT_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.crt
+	CORE_PEER_TLS_ENABLED=true
+	CORE_PEER_MSPCONFIGPATH=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+	CORE_LOGGING_LEVEL=DEBUG
+	CORE_PEER_ADDRESS=peer1.org2.example.com:5051
+
+    export ORDERADDRESS
+    export ORDERER_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE
+    export CORE_PEER_TLS_KEY_FILE
+    export CORE_PEER_LOCALMSPID
+    export CORE_PEER_TLS_CERT_FILE
+    export CORE_PEER_TLS_ENABLED
+    export CORE_PEER_MSPCONFIGPATH
+    export CORE_LOGGING_LEVEL
+    export CORE_PEER_ADDRESS
+    env | grep CORE
+}
+
+CreateFabricNet (){
+    PEER=peer
+
+    setGlobals
+
+    $PEER $*
+
+    # $PEER channel join -b $CHANNEL_NAME.block
+    # $PEER chaincode install $CCPACKAGE
+}
+
+CreateFabricNet $*
