890587030ca06212c54285ce4219722cea4cc92d
Walker <srrain@outlook.com>
2019-06-11 09:39:21 +0800

    update peer.sh

--- first-network/peer.sh
+++ first-network/peer.sh
@@ -11,27 +11,22 @@ export GODEBUG=netdns=go
 export FABRIC_CFG_PATH=$SCRIPT_PATH/sampleconfig
 
 setGlobals () {
-    ORDERADDRESS="orderer.example.com:7050"
-    ORDERER_CA="$SCRIPT_PATH/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-    CORE_PEER_TLS_ROOTCERT_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-	CORE_PEER_TLS_KEY_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.key
-	CORE_PEER_LOCALMSPID=Org1MSP
-	CORE_PEER_TLS_CERT_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.crt
-	CORE_PEER_TLS_ENABLED=true
-	CORE_PEER_MSPCONFIGPATH=$SCRIPT_PATH/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-	CORE_LOGGING_LEVEL=DEBUG
-	CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+    export ORDERADDRESS=orderer.example.com:7050
+    export ORDERER_CA=$SCRIPT_PATH/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+    export CORE_PEER_ID=peer-test
+    export CORE_PEER_TLS_ROOTCERT_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt
+	export CORE_PEER_TLS_KEY_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.key
+	export CORE_PEER_LOCALMSPID=Org2MSP
+	export CORE_PEER_TLS_CERT_FILE=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/server.crt
+	export CORE_PEER_TLS_ENABLED=true
+	export CORE_PEER_MSPCONFIGPATH=$SCRIPT_PATH/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+	export CORE_LOGGING_LEVEL=DEBUG
+	export CORE_PEER_ADDRESS=peer1.org2.example.com:5051
+    export CORE_PEER_LISTENADDRESS=0.0.0.0:5051
+    export CORE_PEER_GOSSIP_ENDPOINT=0.0.0.0:5051
+    export CORE_PEER_EVENTS_ADDRESS=0.0.0.0:5053
+    # export CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:5052
 
-    export ORDERADDRESS
-    export ORDERER_CA
-    export CORE_PEER_TLS_ROOTCERT_FILE
-    export CORE_PEER_TLS_KEY_FILE
-    export CORE_PEER_LOCALMSPID
-    export CORE_PEER_TLS_CERT_FILE
-    export CORE_PEER_TLS_ENABLED
-    export CORE_PEER_MSPCONFIGPATH
-    export CORE_LOGGING_LEVEL
-    export CORE_PEER_ADDRESS
     env | grep CORE
 }
 
